116418778b5d13709adc895ab99070f8fef21581
Sumin0411 <kimsumin411@gmail.com>
2024-09-24 13:21:14 +0900

    ✅ font 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 export default function App(){
   return (
     <>
-    <h1 id = "title" className="title"
-    style={{color : "pink"}}>
-      App Component
-    </h1>
-    <button> click </button>
+    <h1 className=" jeju text-5xl">제주 돌담체</h1>
+    <h1 className=" jeju text-5xl">제주 돌담체</h1>
+    <h1 className=" roboto text-5xl">구글 폰트 적용</h1>
     </>
   )
 }
