2a5dcd1ea7118280b6408ebcdca0bee25570a41e
Sumin0411 <kimsumin411@gmail.com>
2024-09-24 11:41:14 +0900

    ➖ tailwind 밑줄 없애기 & 모바일에서 hover 막기

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import { twMerge } from "tailwind-merge";
-import styled from "styled-components";
-
-const HelloWorld = styled.h1`
-  font-size: 30px;
-  color: #ed4848;
-  text-decoration: line-through;
-  &:hover{
-    color:blue;
-  }
-`;
-export default function App() {
-  const isActive = true;
+export default function App(){
   return (
     <>
-    <h1 
-      className={twMerge(
-        "text-rose-500", 
-        "font-bold",
-        "text-5xl",
-        "text-black"
-      )}
-    >
-      Hello World!
+    <h1 id = "title" className="title"
+    style={{color : "pink"}}>
+      App Component
     </h1>
-    <button
-      className = {twMerge(
-        "p-2 border border-gray-500 rounded-md bg-black text-white",
-        isActive && "bg-blue-500 border-blue-500"
-    )}
-    >
-      클릭
-    </button>
+    <button> click </button>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    module.exports = {
+      future: {
+        hoverOnlyWhenSupported: true,
+      },
+    }
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  
 }
 
